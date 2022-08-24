f5de30bba40fa1e337ce52ff8e25bba5c632831d
Spencer Haka <spencer.haka@gmail.com>
2022-08-23 20:37:37 -0600

    Complete all unicorn tests

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,16 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color = "white") {
     this.name = name;
     this.color = color;
+  }
+  isWhite() {
+    if (this.color === "white") {
+      return true;
+    } else { return false }
+  }
+
+  says(phrase) {
+    return `**;* ${phrase} *;**`;
   }
 }
 
