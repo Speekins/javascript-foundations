8787929d4f943c969510b8c66d09d1cca6b6e2e7
Spencer Haka <spencer.haka@gmail.com>
2022-08-29 20:23:56 -0600

    complete dragon test cases

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,22 @@
+class Dragon {
+  constructor(name, rider){
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.eatTimes = 0;
+  }
 
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+
+  eat() {
+    this.eatTimes += 1;
+    if (this.eatTimes === 3) {
+      this.hungry = false;
+    }
+  }
+}
+
+
+module.exports = Dragon;
