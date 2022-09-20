09bf657276a4a759ef06519db7cf77c7cf6668ac
Spencer Haka <spencer.haka@gmail.com>
2022-09-19 20:18:03 -0600

    complete all ski-lift tests

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -1,7 +1,14 @@
 var Skier = require('./Skier');
 
 class Lift {
+  constructor(limit) {
+    this.limit = limit
+    this.inService = true;
+    this.skiers = [];
+    this.safetyBar = 'up';
+  }
   
+  admitSkier(name, )
 }
 
 module.exports = Lift;
--- ski-lift/src/Skier.js
+++ ski-lift/src/Skier.js
@@ -1,5 +1,21 @@
 class Skier {
-
+  constructor(name, liftTicket) {
+    this.name = name;
+    this.hasLiftTicket = liftTicket;
+    this.skillLevel = 1;
+    this.nextSlope = 'green circle';
+  }
+  takeLesson() {
+    this.skillLevel++
+  } 
+  pickSlope() {
+    if (this.skillLevel > 2 && this.skillLevel < 5) {
+    this.nextSlope = 'blue square'
+    }
+    if (this.skillLevel > 4) {
+      this.nextSlope = 'black diamond'
+    }
+  }
 }
 
 module.exports = Skier;
