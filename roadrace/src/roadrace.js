35d803f3d230712699fc8fbfd1409509933eb94e
Spencer Haka <spencer.haka@gmail.com>
2022-09-21 09:45:13 -0600

    Complete all roadrace tests

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,26 @@
 class Roadrace {
+  constructor(race) {
+    this.name = race.title;
+    this.location = race.city;
+    this.distance = 0;
+    this.participants = [];
+  }
 
+  setDistance(miles) {
+    this.distance = miles;
+    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`
+  }
+
+  registerParticipants(participant) {
+    this.participants.push(participant);
+  }
+
+  completeRace() {
+    for (var i = 0; i < this.participants.length; i++) {
+      var participant = this.participants[i];
+      participant.runRace(this.name, this.distance);
+    }
+  }
 }
 
 module.exports = Roadrace;
